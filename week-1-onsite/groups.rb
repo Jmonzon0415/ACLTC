9d777e17827213ce84b3852ed626bc30dd0d7c52
Joshua Monzon <Jmonzon0415@gmail.com>
2016-01-06 18:57:28 -0500

    finished groups

--- week-1-onsite/groups.rb
+++ week-1-onsite/groups.rb
@@ -1,13 +1,40 @@
+puts "Enter Students Names"
+puts "Type Done when complete "
+
 classroom = Array.new 
 
-4.times do
+input = gets.chomp
 
-puts "Enter Students Names"
+while input != 'Done'.to_s
+  classroom << input
+  input = gets.chomp.to_s
+end
 
-classroom << gets.chomp 
+ 
+mixed_class = classroom.shuffle!
 
-end 
 
-p classroom
+until mixed_class.length == 0 
+  if mixed_class.length == 3 
+    group = mixed_class.pop(3)
+  else
+    group = mixed_class.pop(2)
+  end
+  print "Group: "
+  group.each do |member | 
+    print "#{member} "
+  end
+  puts ""
+end
+
+
+
+
+
+
+
+
+
+
 
 
